a789f7b249c439441feccc9aaf28f2a31f9b25cc
Mikhail Kedzel <mishanx873@gmail.com>
2019-11-18 20:51:41 +0300

    feat : improve styles, add a login button

--- codejam-image-api/src/index.js
+++ codejam-image-api/src/index.js
@@ -1,5 +1,10 @@
 import './scss/main.scss';
+import netlifyIdentity from 'netlify-identity-widget';
 import Canvas from './js/canvas';
+
+netlifyIdentity.init({
+  container: '#netlify-modal', // defaults to document.body,
+});
 
 const canvas = new Canvas();
 
@@ -10,7 +15,6 @@ document.getElementById('searchSubmit').addEventListener('click', async (event) 
   const url = `https://api.unsplash.com/photos/random?query=town,${city}&client_id=f809e76f3410e5f384bfce6a5dd6a9a516bb8f3e16797e65428f16558f9d4b37`;
   const response = await fetch(url);
   const json = await response.json();
-  console.log(json.urls.full);
   canvas.drawImageFromUrl(json.urls.full);
 });
 // Needed for Hot Module Replacement
